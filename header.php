f075509fd88ef531451288e50183fa48af18d67c
ncodignotto <ncodignotto@liquidnet.com>
2012-11-26 21:40:55 -0800

    Removed Bootstrap..

--- header.php
+++ header.php
@@ -4,18 +4,20 @@
  * @subpackage Default_Theme
  */
 ?>
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <head>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
 <!-- Bootstrap first, then we override -->
+<!--
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory');?>/bootstrap-min.css"/>
+-->
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <!-- SyntaxHighlighter Support -->
-<link rel="stylesheet" href="/blog/wp-content/plugins/syntaxhighlighter/syntaxhighlighter3/styles/shCore.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="/blog/wp-content/plugins/syntaxhighlighter/syntaxhighlighter3/styles/shThemeDefault.css" type="text/css" media="screen" />
-<script type="text/javascript" src="/blog/wp-content/plugins/syntaxhighlighter/syntaxhighlighter3/scripts/shCore.js"></script> 
+<link rel="stylesheet" href="/wp-content/plugins/syntaxhighlighter/syntaxhighlighter3/styles/shCore.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="/wp-content/plugins/syntaxhighlighter/syntaxhighlighter3/styles/shThemeDefault.css" type="text/css" media="screen" />
+<script type="text/javascript" src="/wp-content/plugins/syntaxhighlighter/syntaxhighlighter3/scripts/shCore.js"></script>
 
 <link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
 
