09636d70a214176ea248175cb4515f83aff79e72
ncodignotto <ncodignotto@liquidnet.com>
2014-02-23 20:52:10 -0800

    Changed stuff

--- header.php
+++ header.php
@@ -5,9 +5,12 @@
  */
 ?>
 <!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+
+<link rel="alternate" type="application/rss+xml" title="nickcoding.com" href="<?php bloginfo('rss2_url'); ?>"/>
 
 <!-- Bootstrap first, then we override -->
 <!--
